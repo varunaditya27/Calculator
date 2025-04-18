a52c8d638a98c5fc23c08b14fd9be957168879ca
varunaditya27 <varunaditya2706@gmail.com>
2025-04-18 08:44:34 +0530

    modified the css file

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ let display_screen = document.getElementById("display-screen");
 let clear_button = document.getElementById("clear-button");
 let equals_button = document.getElementById("equals-to");
 let result_displayed = false;
+let clear_screen = true;
+let backspace_button = document.getElementById("backspace-button")
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
@@ -12,7 +14,14 @@ buttons.forEach(button => {
             display_screen.innerText += button.innerText;
         }
         else {
-            display_screen.innerText += button.innerText;
+            if (clear_screen) {
+                display_screen.innerText = "";
+                display_screen.innerText += button.innerText;
+                clear_screen = false;
+            }
+            else {
+                display_screen.innerText += button.innerText;
+            }
         }
     })
 });
@@ -25,4 +34,16 @@ equals_button.addEventListener("click", () => {
 
 clear_button.addEventListener("click", () => {
     display_screen.innerText = "0";
+});
+
+backspace_button.addEventListener("click", () => {
+    if (!clear_screen) {
+        if (display_screen.innerText.length == 1) {
+            display_screen.innerText = "0";
+            clear_screen = true;
+        }
+        else {
+            display_screen.innerText = display_screen.innerText.slice(0, -1); 
+        }
+    }
 });
